089f346b5e84f73a2283570517549250fa15d6d1
LCN <3334673511@qq.com>
2023-09-12 19:55:26 +0800

    Initial commit

---
+++ src/main/java/com/book/utils/ThymeleafUtil.java
@@ -0,0 +1,22 @@
+package com.book.utils;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+public class ThymeleafUtil {
+    private static final TemplateEngine engine;
+    static  {
+        engine = new TemplateEngine();
+        ClassLoaderTemplateResolver r = new ClassLoaderTemplateResolver();
+        r.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        engine.setTemplateResolver(r);
+    }
+
+    public static void process(String template, IContext context, Writer writer){
+        engine.process(template,context,writer);
+    }
+}
